0f83f01b1f105b7eee49104d405841c8e3b10480
Tomas Poveda <tpovedatd@gmail.com>
2021-03-27 20:06:39 +0100

    Fixed tests

--- tests/test_general.py
+++ tests/test_general.py
@@ -7,7 +7,7 @@ Module that contains general tests for tpDcc-libs-svg
 
 from tpDcc.libs.unittests.core import unittestcase
 
-from tpDcc.libs.nameit import __version__
+from tpDcc.libs.svg import __version__
 
 
 class VersionTests(unittestcase.UnitTestCase(as_class=True), object):
